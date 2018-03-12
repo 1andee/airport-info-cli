0c178ce73375f8360779a48f7051ec21d6b88dfc
Ian Duke <ian.c.duke@gmail.com>
2018-03-11 22:11:40 -0400

    convert to CLI tool

--- express_server.js
+++ express_server.js
@@ -1,9 +1,5 @@
-const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
 const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const readlineSync = require('readline-sync');
 const icao = require('icao');
 const airportDiagrams = require('airport-diagrams');
 const notams = require('notams');
@@ -12,81 +8,66 @@ const metarFetcher = new MetarFetcher();
 const TafFetcher = require('metar-taf').TafFetcher;
 const tafFetcher = new TafFetcher();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+let input = readlineSync.question('Please enter an ICAO code: ');
+let airport = input.toUpperCase();
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+if (!icao[airport]) {
+  console.log("#####\nAirport not found");
+  console.log("Please try another search\n#####");
+  return;
+} else {
+
+  console.log(`----`);
+  console.log(`\n${airport}\n`);
+
+  metarFetcher.getData(airport)
+  .then((response) => {
+    console.log(`############################`);
+    console.log(`            METAR`);
+    console.log(`############################`);
+    console.log(response);
+  }), ((error) => {
+    console.error(error);
+  });
 
 
-app.post("/chart", (req, res) => {
-  let input = req.body['airport']
-  let airport = input.toUpperCase();
+  tafFetcher.getData(airport)
+  .then((response) => {
+    console.log(`############################`);
+    console.log(`            TAF`);
+    console.log(`############################`);
+    console.log(response);
+  }), ((error) => {
+    console.error(error);
+  });
 
-  if (!icao[airport]) {
-    console.log("#####\nAirport not found");
-    console.log("Please try another search\n#####");
-    return;
-  } else {
+  // Displays link to Airport Chart PDF if it's a U.S. airport
+  const geoCheck = (airport) => {
+    let firstChar = airport.substring(0, 1);
+    if (firstChar === 'K' || firstChar === 'P') {
+      airportDiagrams.list(airport)
+      .then(results => {
+        let result = results[0];
+        let plateURL = (result['procedure']['url']);
+        revisedURL = plateURL.substring(0, plateURL.length - 16);
+        console.log(`############################`);
+        console.log(`    AIRPORT DIAGRAM (PDF)`);
+        console.log(` ${result['airport']} / ${result['ident']}`);
+        console.log(`############################`);
+        console.log(`${revisedURL}\n`);
+      })
+    } else { return };
+  };
+  geoCheck(airport);
 
-    console.log(`----`);
-    console.log(`\n${airport}\n`);
-
-    metarFetcher.getData(airport)
-    .then((response) => {
-      console.log(`############################`);
-      console.log(`            METAR`);
-      console.log(`############################`);
-      console.log(response);
-    }), ((error) => {
-      console.error(error);
+  notams(airport, { format: 'DOMESTIC' })
+  .then(results => {
+    console.log(`############################`);
+    console.log(`           NOTAMS`);
+    console.log(`############################`);
+    var notams = results[0]['notams'];
+    notams.forEach((notam) => {
+      console.log(notam);
     });
-
-
-    tafFetcher.getData(airport)
-    .then((response) => {
-      console.log(`############################`);
-      console.log(`            TAF`);
-      console.log(`############################`);
-      console.log(response);
-    }), ((error) => {
-      console.error(error);
-    });
-
-    // Displays link to Airport Chart PDF if it's a U.S. airport
-    const geoCheck = (airport) => {
-      let firstChar = airport.substring(0, 1);
-      if (firstChar === 'K' || firstChar === 'P') {
-        airportDiagrams.list(airport)
-        .then(results => {
-          let result = results[0];
-          let plateURL = (result['procedure']['url']);
-          revisedURL = plateURL.substring(0, plateURL.length - 16);
-          console.log(`############################`);
-          console.log(`    AIRPORT DIAGRAM (PDF)`);
-          console.log(` ${result['airport']} / ${result['ident']}`);
-          console.log(`############################`);
-          console.log(`${revisedURL}\n`);
-        })
-      } else { return };
-    };
-    geoCheck(airport);
-
-    notams(airport, { format: 'DOMESTIC' })
-    .then(results => {
-      console.log(`############################`);
-      console.log(`           NOTAMS`);
-      console.log(`############################`);
-      var notams = results[0]['notams'];
-      notams.forEach((notam) => {
-        console.log(notam);
-      });
-    });
-
-  }
-
-  res.redirect("/");
-
-});
+  });
+};
